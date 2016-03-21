b504ac39f7c21f271931b1348c14509666603f7a
shinyorke <spirits.is.my.rader@gmail.com>
2016-03-21 15:13:08 +0900

    Add to hisashi iwakuma example

---
+++ pitchpx/const.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+__author__ = 'Shinichi Nakagawa'
+
+
+class PitchPxConst(object):
+
+
+    # 球種はこのリンクを参照 http://pitchfx.texasleaguers.com/
+    PITCH_TYPES = {
+        'FA': 'Fastball',
+        'FF': 'four-seam Fastball',
+        'FT': 'two-seam Fastball',
+        'FC': 'Cut Fastball',
+        'FS': 'Split-finger Fastball',
+        'FO': 'Forkball',
+        'SI': 'Sinker',
+        'SL': 'Slider',
+        'CU': 'Curveball',
+        'KC': 'Knuckle Curve',
+        'EP': 'Ephuus',
+        'CH': 'Change-up',
+        'SC': 'Screwball',
+        'KN': 'Knuckleball',
+        'UN': 'Unknown',
+    }

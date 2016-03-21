5f01f6f0134edf4e286932070e537b79f2a7d1c0
shinyorke <spirits.is.my.rader@gmail.com>
2016-03-21 18:18:09 +0900

    Add to pitch type

--- pitchpx/const.py
+++ pitchpx/const.py
@@ -25,3 +25,41 @@ class PitchPxConst(object):
         'KN': 'Knuckleball',
         'UN': 'Unknown',
     }
+
+    # markers(matplotlib)
+    PITCH_TYPES_MARKERS = {
+        'FA': 'o',
+        'FF': '.',
+        'FT': ',',
+        'FC': '6',
+        'FS': '7',
+        'FO': '7',
+        'SI': '5',
+        'SL': '4',
+        'CU': '7',
+        'KC': ',',
+        'EP': 'D',
+        'CH': 'o',
+        'SC': 'H',
+        'KN': '*',
+        'UN': 'x',
+    }
+
+    # colors(matplotlib)
+    PITCH_TYPES_COLORS = {
+        'FA': 'r',
+        'FF': 'r',
+        'FT': 'r',
+        'FC': 'r',
+        'FS': 'r',
+        'FO': 'b',
+        'SI': 'b',
+        'SL': 'b',
+        'CU': 'g',
+        'KC': 'g',
+        'EP': 'g',
+        'CH': 'g',
+        'SC': 'g',
+        'KN': 'g',
+        'UN': 'g',
+    }
